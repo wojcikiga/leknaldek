6af27101b865fdf3a6f450436a173d44cb723aac
Łukasz Woźny <kontakt@lukaszwozny.com>
2020-05-15 12:40:45 +0200

    update biale kolka view

--- assets/js/pytanie.js
+++ assets/js/pytanie.js
@@ -1,66 +1,82 @@
-
-
 var config = {
-			type: 'doughnut',
-			data: {
-              
-				datasets: [{
-					data: [
-						12,5
-					
-					],
-					backgroundColor: [
-						'#82DEFE',
-                        'transparent',
-                        
-						
-					],
-                    borderWidth:0,
-                  
-					label: 'Dataset 1'
-				}],
-// 				labels: [
-// 					'Red',
-// 					'Orange',
-// 					'Yellow'
-					
-// 				]
-			},
-			options: {
-				responsive: false,
-                 cutoutPercentage:70,
-                aspectRatio: 1,
-                layout: {
-                    padding: {
-                        left: 0,
-                        right: 0,
-                        top: -10,
-                        bottom: 0
-                    }
-                },
-			
-				// title: {
-				// 	display: true,
-				// 	text: 'Chart.js Doughnut Chart'
-				// },
-				animation: {
-					animateScale: true,
-					animateRotate: true
-				}
+	type: 'doughnut',
+	data: {
+		datasets: [{
+			data: [
+				12, 5
+			],
+			backgroundColor: [
+				'#82DEFE',
+				'transparent',
+			],
+			borderWidth: 0,
+
+			label: 'Dataset 1'
+		}],
+	},
+	options: {
+		responsive: true,
+		cutoutPercentage: 70,
+		aspectRatio: 1,
+		layout: {
+			padding: {
+				left: 0,
+				right: 0,
+				top: -10,
+				bottom: 0
 			}
-		};
+		},
+		animation: {
+			animateScale: true,
+			animateRotate: true
+		}
+	}
+};
+
+var config2 = {
+	type: 'doughnut',
+	data: {
+		datasets: [{
+			data: [
+				3, 6, 2
+			],
+			backgroundColor: [
+				'red',
+				'#67ee3e',
+				'transparent',
+			],
+			borderWidth: 0,
+
+			label: 'Dataset 1'
+		}],
+	},
+	options: {
+		responsive: true,
+		cutoutPercentage: 70,
+		aspectRatio: 1,
+		layout: {
+			padding: {
+				left: 0,
+				right: 0,
+				top: -10,
+				bottom: 0
+			}
+		},
+		animation: {
+			easing: 'easeInOutQuad',
+			duration: 1000,
+			animateScale: true,
+			animateRotate: true
+		}
+	}
+};
 
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+	var ctx = $('#result_chart')
+	var chart = new Chart(ctx, config)
+	var ctx2 = $('#result_chart2')
+	var chart2 = new Chart(ctx2, config2)
 
-    
-    console.log('elo')
-    var ctx = $('#result_chart')
-    var chart = new Chart(ctx,config)
-    
-    
-//     chart.canvas.parentNode.style.height = '128px';
-// chart.canvas.parentNode.style.width = '128px';
-    
+
 })
